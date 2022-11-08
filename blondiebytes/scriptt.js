ccb0c35bbedbb38b67c779bae872ca8ec4ea692f
alannarisse <angrypirate@gmail.com>
2022-11-07 20:07:20 -0800

    typoes and weird smart quotes

--- blondiebytes/scriptt.js
+++ blondiebytes/scriptt.js
@@ -1,13 +1,12 @@
 function revealMessage() {
-    document.getElementById("hiddenMessage").style.display ='block';
+  document.getElementById('hiddenMessage').style.display = 'block'
 }
 
 function countDown() {
-    var currentVal = document.getElementById("countDownButtonId").innerHTML;
-    var newVal = 0;
-    if(currentVal > 0) {
-        newVal = currentVal -1;
-    }
-    document.getElementById("countDownButton").innerHTML = newVal;
-
+  var currentVal = document.getElementById('countDownButton').innerHTML
+  var newVal = 0
+  if (currentVal > 0) {
+    newVal = currentVal - 1
+  }
+  document.getElementById('countDownButton').innerHTML = newVal
 }
